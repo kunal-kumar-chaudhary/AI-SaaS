f87e6bf24eb5d28eb367edd77e647ae50799734b
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-06-30 02:34:05 +0530

    added webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,6 +1,10 @@
 import {headers} from "next/headers";
 import Stripe from "stripe";
 import { stripe } from "@/lib/stripe";
+import { db } from "@/lib/db";
+import { userSubscriptions } from "@/lib/db/schema";
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
 export async function POST(req: Request){
     const body = await req.json();
     // we need to validate whether the hook is coming from the stripe
@@ -20,8 +24,30 @@ export async function POST(req: Request){
     // if the new subscription is successful, we will save it to database
     if (event.type === "checkout.session.completed") {
         const subscription = await stripe.subscriptions.retrieve(session.subscription as string);
-        if (!session.metadata.userId){
-
+        if (!session?.metadata?.userId){
+            return new Response("User not found", {status: 404});
         }
+        // if everthing is fine, we will save the subscription to the database
+        await db.insert(userSubscriptions).values({
+            userId: session.metadata.userId,
+            stripeSubscriptionId: subscription.id,
+            stripeCustomerId: subscription.customer as string,
+            stripePriceId: subscription.items.data[0].price.id,
+            stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        });
     }
+
+    if (event.type === "invoice.payment_succeeded"){
+        // if the payment is successful, we will update the subscription
+        const subscription = await stripe.subscriptions.retrieve(session.subscription as string);
+        if (!session?.metadata?.userId){
+            return new Response("User not found", {status: 404});
+        }
+        await db.update(userSubscriptions).set({
+            stripePriceId: subscription.items.data[0].price.id,
+            stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        }).where(eq(userSubscriptions.stripeSubscriptionId, subscription.id));
+    }
+
+    return NextResponse.json(null, {status: 200})
 }
